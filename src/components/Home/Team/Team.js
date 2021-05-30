a81002da2dba16b4cf1d5cab85657f6bf64ef865
Jahid Hasan <slniloy500@gmail.com>
2021-05-30 12:10:50 +0600

    change redmi

--- src/components/Home/Team/Team.js
+++ src/components/Home/Team/Team.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import TeamDetail from './TeamDetail';
+
+import jahidImg from '../../../images/admin2.20a81b41.png';
+import asrafulImg from '../../../images/asraful.png';
+import sakibImg from '../../../images/sakib.jpg';
+
 const teamData = [
     {
         id:1,
         name:'Md Jahid Hasan',
         position:'Web Developer',
-        
+        img:jahidImg
     },
     {
         id:2,
         name:'Ashraful Hasan Zayed',
-        position:'Web Developer'
+        position:'Web Developer',
+        img:asrafulImg
     },
     {
         id:3,
         name:'Sakib Hasan',
-        position:'Project Manager'
+        position:'Project Manager',
+        img:sakibImg
     }
 ]
 const Team = () => {
     return (
         <div className="py-5 text-center" style={{ backgroundColor:'rgb(231, 231, 231)'}}>
             <p className="mb-0 h6">LEADERSHIP <span className="text-warning">TEAM</span></p>
-            <h1 className="fw-bold">Meet The Expert Team</h1>
+            <h1 className="fw-bold my-4">Meet The Expert Team</h1>
             <div className="container">
                 <div className="row mt-3">
                     {
--- src/components/Home/Team/TeamDetail.js
+++ src/components/Home/Team/TeamDetail.js
@@ -3,7 +3,13 @@ import React from 'react';
 const TeamDetail = ({teamMember}) => {
     return (
         <div className="col-md-4 col-sm-6">
-            <h2>HH</h2>
+            <div class="card">
+                <img src={teamMember.img} class="card-img-top img-100" alt={teamMember.name}/>
+                <div class="card-body">
+                <h4 class="card-title">{teamMember.name}</h4>
+                <p className="text-warning h5 ">{teamMember.position}</p>
+                </div>
+            </div>
         </div>
     );
 };
